cf424f604b46948ba34498da87fe19c5e9f4e5ba
jasmosez <jas.schaffer@gmail.com>
2019-11-18 17:26:11 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+  ## setting
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+ 
+  ## getting
+  def name
+    @this_dogs_name
+  end
+
+end
+
+
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+
